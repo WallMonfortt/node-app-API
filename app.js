54ac29b115133af62dcf00038aa13eef1f2b9f28
Wal Monfortt <jose.gual.monfortte.f@gmail.com>
2021-09-30 09:40:06 -0500

    manejo de ruta de inicio

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.use("/api/v1", (req, res) =>{
+app.get("/", (req, res) =>{
+  res.json({
+    message: "Bienvenido a mi App"
+  });
+});
+
+app.get("/api/v1", (req, res) =>{
   res.json({
     message: "Testing Heroku Deploy"
   });
 });
 
-app.use("/api/v1/users", (req, res) =>{
+app.get("/api/v1/users", (req, res) =>{
   res.json({
     id:1,
     firstname: "Academlo",
